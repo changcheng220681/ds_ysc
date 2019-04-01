e22fafe43e2cda0fe256a66a1edb0f77d18d32b2
linhua <18722719931@163.com>
2019-04-01 12:52:01 +0800

    修改了必然的controller   --林华第一次同步

--- pinyougou-web-portal/src/main/java/cn/itcast/core/controller/BrandController123.java
+++ pinyougou-web-portal/src/main/java/cn/itcast/core/controller/BrandController123.java
@@ -4,5 +4,6 @@ public class BrandController123 {
     public static void main(String[] args) {
         System.out.println("1234");
         System.out.println("2234");
+        System.out.println("2234");
     }
 }
