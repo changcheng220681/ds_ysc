215cc1dcd172a8cee20d92900616fae09d378045
linhua <18722719931@163.com>
2019-04-01 12:28:35 +0800

    林华第一次同步

---
+++ pinyougou-web-portal/src/main/java/cn/itcast/core/controller/BrandController123.java
@@ -0,0 +1,8 @@
+package cn.itcast.core.controller;
+
+public class BrandController123 {
+    public static void main(String[] args) {
+        System.out.println("1234");
+        System.out.println("2234");
+    }
+}
